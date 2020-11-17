74719cd5f018fed7bc4d8701e5f37b26be646a0d
Haoning Chen <owenchen97@cs.uchicago.edu>
2020-11-16 19:33:23 -0600

    Add arg, argf and main in library.

--- lib-llvm.cpp
+++ lib-llvm.cpp
@@ -1,5 +1,6 @@
 /* Library functions for EK language. */
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -15,5 +16,34 @@ void printFloat(float f) {
     cout << f << endl;
 }
 
-int getarg(int i) {}
-float getargf(int i) {}
+
+/* Variables that store argument information. */
+static int argcount;
+static char **argstrings;
+
+int getarg(int i) {
+    if (i < 0 || i >= argcount) {
+        cout << "Error: getarg()'s index out of bound.\n";
+        exit(-1);
+    }
+
+    return atoi(argstrings[i]);
+}
+
+float getargf(int i) {
+    if (i < 0 || i >= argcount) {
+        cout << "Error: getargf()'s index out of bound.\n";
+        exit(-1);
+    }
+
+    return atof(argstrings[i]);
+}
+
+extern "C" void run();
+
+int main(int argc,char *argv[]) {
+    argcount = argc - 1;
+    argstrings = argcount > 0 ? argv + 1 : NULL;
+
+    run();
+}
